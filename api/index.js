46f9594675ad5c1c73921f049a6b34da22b1c17f
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2025-08-06 18:45:10 -0600

    got basic game
    TODO: end game and game over screens
    just got an alert rn

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 export default async function transliterateToEgyptian(req, res) {
 	try {
+
+		// CORS allow all to make this a public API
+		res.setHeader("Access-Control-Allow-Origin", "*");
+
 		const { searchParams } = new URL(process.env.NEXT_PUBLIC_PROCESSING_SERVER + req.url);
 
 		// get english from url search param
@@ -9,16 +13,19 @@ export default async function transliterateToEgyptian(req, res) {
 		const response = await fetch("https://open.spotify.com/embed/playlist/" + playlistId);
 		const embedPage = await response.text();
 
-		// extract and parse playlist data json
-		const playlistJson = JSON.parse(embedPage.split("type=\"application/json\">")[1].split("</script>")[0]);
+		// extract and parse page data json
+		const pageJson = JSON.parse(embedPage.split("type=\"application/json\">")[1].split("</script>")[0]);
+		const rawTracks = pageJson.props.pageProps.state.data.entity.trackList;
 
 		// get random song from tracks
-		const trackList = playlistJson.props.pageProps.state.data.entity.trackList;
-		const randomSong = trackList[Math.floor(Math.random() * trackList.length)];
+		const randomSong = rawTracks[Math.floor(Math.random() * rawTracks.length)];
+
+		// format track list
+		const trackList = rawTracks.map(track => track.title + " by " + track.subtitle);
 
 		// send output
 		res.setHeader("Content-Type", "application/json;");
-		res.status(200).send(randomSong);
+		res.status(200).send({ randomSong, trackList });
 	} catch (error) {
 		console.log(error);
 		// send output
---
+++ app/page.js
@@ -0,0 +1,179 @@
+"use client";
+
+import FilterSelect from "@/src/components/FilterSelect";
+import LoadingBar from "@/src/components/LoadingBar";
+import { useRef, useState } from "react";
+
+// segments to split the song into
+const segments = [1, 2, 4, 7, 11, 16];
+
+export default function Page() {
+	const [page, setPage] = useState("home");
+	const [playlistLink, setPlaylistLink] = useState("");
+	const [audioPercentage, setAudioPercentage] = useState(0);
+	const [progressLabel, setProgressLabel] = useState("");
+	const [songAudio, setSongAudio] = useState("");
+
+	const songData = useRef(null);
+	const audio = useRef(null);
+	const guess = useRef(null);
+
+	const segmentIndex = useRef(0);
+	const trackList = useRef(null);
+
+	return (
+		<main>
+
+			<h1>Playlist Heardle</h1>
+
+			{
+				// home page
+				page == "home" &&
+				<section>
+					<p>Enter a Spotify playlist link (make sure it's an open.spotify.com link) and see how well you know the songs. You'll have to guess a random song from a short preview.</p>
+
+					<br />
+
+					<input placeholder="Playlist link" value={playlistLink} onChange={(data) => setPlaylistLink(data.target.value)} />
+
+					<br />
+
+					<button onClick={startGame}>Play</button>
+
+					<br />
+
+				</section>
+			}
+
+			{
+				// game page
+				page == "game" &&
+				<section>
+
+					<GameInstructions />
+
+					<br />
+					<br />
+
+					<audio ref={audio} src={songAudio} onTimeUpdate={timeUpdate} autoPlay />
+
+					<LoadingBar percentage={audioPercentage} label={progressLabel} />
+
+					<br />
+
+					<div style={{ display: "flex", gap: "10px" }}>
+						<button onClick={play}>Play</button>
+						<button onClick={skip}>Skip</button>
+					</div>
+
+					<br />
+					<br />
+
+
+					<div style={{ display: "flex", gap: "10px" }}>
+						<FilterSelect
+							placeholder="Guess a song/artist"
+							options={trackList.current}
+							style={{ width: "17em" }}
+							onChange={(event) => guess.current = event.target.value}
+							onSelect={(option) => guess.current = option}
+						/>
+						<button onClick={sendGuess}>Guess</button>
+					</div>
+
+				</section>
+			}
+		</main>
+	);
+
+	function GameInstructions() {
+		const audioLimit = segments[segmentIndex.current];
+
+		// plural ending for second/seconds
+		const pluralEnding = segments[segmentIndex.current] == 1 ? "" : "s";
+
+		return <p>Try guessing the song from the first {audioLimit} second{pluralEnding} of the preview. Skip forward if you can't get it yet.</p>;
+	}
+
+	async function startGame() {
+		// change page
+		setPage("game");
+
+		// get id from playlist link
+		const playlistId = playlistLink.split("/playlist/")[1].split("/")[0];
+
+		// get playlist data
+		const response = await fetch(process.env.NEXT_PUBLIC_PROCESSING_SERVER + "/api/projects/playlistHeardle?playlist=" + playlistId);
+		const data = await response.json();
+
+		// store random song data
+		songData.current = data.randomSong;
+		setSongAudio(data.randomSong.audioPreview.url);
+
+		// store track list
+		trackList.current = data.trackList;
+	}
+
+	function play() {
+		audio.current.currentTime = 0;
+		audio.current.play();
+	}
+
+	function skip() {
+		if (segmentIndex.current < segments.length - 1) {
+			// move to next segment
+			segmentIndex.current++;
+
+			play();
+		}
+	}
+
+	function sendGuess() {
+		const realSong = songData.current.title + " by " + songData.current.subtitle;
+
+		// TODO: actual end game
+		if (guess.current == realSong) {
+			alert("hip hip hooray");
+		} else {
+			skip();
+		}
+	}
+
+	function timeUpdate() {
+		setProgressLabel(
+			formatSeconds(audio.current.currentTime)
+			+ "/" +
+			formatSeconds(segments[segments.length - 1])
+		);
+
+		// update loading bar percentage
+		const percentage = audio.current.currentTime / segments[segments.length - 1];
+		setAudioPercentage(percentage);
+
+		const audioLimit = segments[segmentIndex.current];
+
+		// check if audio limit passed or met
+		if (audio.current.currentTime >= audioLimit) {
+			// pause and limit
+			audio.current.pause();
+			audio.current.currentTime = audioLimit;
+		}
+	}
+
+	function formatSeconds(seconds) {
+		if (isNaN(seconds) || seconds < 0) {
+			return "00:00";
+		}
+
+		const hrs = Math.floor(seconds / 3600);
+		const mins = Math.floor((seconds % 3600) / 60);
+		const secs = Math.floor(seconds % 60);
+
+		const pad = (n) => String(n).padStart(2, "0");
+
+		return hrs > 0
+			? `${pad(hrs)}:${pad(mins)}:${pad(secs)}`
+			: `${pad(mins)}:${pad(secs)}`;
+	}
+
+}
